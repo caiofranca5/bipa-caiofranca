b19a5c363c50dc3c34fe1f7b5b1953034763c787
Caio Franca <caiofranca5@hotmail.com>
2025-02-03 16:39:29 -0300

    Initial Commit

---
+++ bipa-caiofranca/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bipa-caiofranca
+//
+//  Created by Caio França on 03/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bipa-caiofranca/bipa_caiofrancaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bipa_caiofrancaApp.swift
+//  bipa-caiofranca
+//
+//  Created by Caio França on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct bipa_caiofrancaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
